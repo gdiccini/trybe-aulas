aae8886c5b0d0708daf78ce79ce247146d1abfb4
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-10-06 10:40:21 -0300

    video example cep

--- 29.1-v2/cep.js
+++ 29.1-v2/cep.js
@@ -1,20 +1,46 @@
 const { default: axios } = require('axios');
 
-async function validaeconsultaCEP() {
-  const cep = '46.860-000';
+const CIDADES = {
+  tatui: {
+    cep: '18.271-170',
+  },
+  itaberaba: {
+    cep: '46.880-000',
+  },
+};
 
+function validaCEP(cep) {
   const regexCEP = /^[0-9]{2}.[0-9]{3}-[0-9]{3}$/;
-  const valida = regexCEP.test(cep);
+  return regexCEP.test(cep);
+}
+
+async function brasilAPI(cep) {
+  const URL = `https://brasilapi.com.br/api/cep/v1/${cep}`;
+
+  const request = await axios.get(URL);
+  console.log(request.data);
+}
+
+async function viaCEP(cep) {
+  const URL = `https://viacep.com.br/ws/${cep}/json/`;
+
+  const request = await axios.get(URL);
+  console.log(request.data);
+}
+
+async function consultaCEP(cidade, service) {
+  const { cep } = CIDADES[`${cidade}`];
+
+  const valida = validaCEP(cep);
 
   let cepTratado;
   if (valida) {
     cepTratado = cep.replace(/[^\d]+/g, '');
 
-    const URL = `https://brasilapi.com.br/api/cep/v1/${cepTratado}`;
+    service(cepTratado);
 
-    const request = await axios.get(URL);
-    console.log(request.data);
+    // console.log(request.data);
   }
 }
 
-validaeconsultaCEP();
+consultaCEP('tatui', brasilAPI);
